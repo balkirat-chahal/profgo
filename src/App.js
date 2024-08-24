3388004829b31fccfbb21ac9d789c810dc99c37e
balkirat-chahal <balkirat.chahal@gmail.com>
2024-08-24 12:14:50 -0600

    Created react app and added tailwindcss

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-blue-800'>ProGo</p>
+    </div>
+  );
+}
+
+export default App;
