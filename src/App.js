f7ebc24aab9c17491313ee2a3061074dd248d7de
Brar's Mac <brarsmac@Brarss-MacBook.local>
2024-08-24 13:48:07 -0600

    initialized card.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ProfGoLogo from './components/ProfGoLogo';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className='w-full min-h-screen bg-black'>
       <ProfGoLogo/>
-      <p>Hello</p>
+      <div className='Body flex flex-wrap justify-center'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        
+
+      </div>
     </div>
   )
 }
