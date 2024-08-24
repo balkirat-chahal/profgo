e942e7275e010afafb2a0adea962653da7a6a72a
karan brar <brarkaran4082@gmail.com>
2024-08-24 14:35:29 -0600

    added material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ProfGoLogo from './components/ProfGoLogo';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,11 @@
 import React from 'react'
-
+import { Avatar } from '@mui/material'
+import pokiball from '../assets/pokiball.png'
 function Card() {
   return (
     <div className='border-2 w-56 h-72 rounded-md m-5 mb-20 mt-10 mr-10'>
-      <div className=''>
-        
-
-
+      <div className='p-2 h-40 w-36'>
+      <img src={pokiball} alt="Girl in a jacket" width="300" height="600"/>
       </div>
 
     </div>
