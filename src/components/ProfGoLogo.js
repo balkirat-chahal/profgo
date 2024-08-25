fcc159c047638be1e053d6f22de54adbe4fa38d4
karan brar <brarkaran4082@gmail.com>
2024-08-25 14:59:06 -0600

    changes to logo

--- src/components/ProfGoLogo.js
+++ src/components/ProfGoLogo.js
@@ -6,17 +6,20 @@ import { Link } from 'react-router-dom';
 const ProfGoLogo = () => {
   return (
     <Link to='/'>
-    <div className="flex flex-col justify-center items-center bg-none">
-      <h1 className="text-4xl font-bold text-green-500 flex items-center">
-        Prof<span className="OG text-yellow-500 mr-0">G</span>
-        <span className="inline-block mx-2 relative ml-0 pl-0">
-          <img src={pokiball} alt="Pokéball" className=" poki w-12 h-12 animate-spin-slow pl-0" />
-        </span>
-      </h1>
-    </div>
+      <div className="flex flex-col items-center bg-none">
+        <h1 className="text-4xl font-bold text-green-500 flex items-center">
+          Prof<span className="OG text-yellow-500">G</span>
+          <span className="inline-block mx-2 relative">
+            <img src={pokiball} alt="Pokéball" className="w-12 h-12 animate-spin-slow" />
+          </span>
+        </h1>
+        <div className="text-sm text-white ">
+         Know your profs—quick and easy!
+        </div>
+      </div>
     </Link>
   );
 };
 
-
 export default ProfGoLogo;
+
