25e4327730db63f86c10a31ebbd1ee835e2f5700
karan brar <brarkaran4082@gmail.com>
2024-08-25 11:35:05 -0600

    Merge branch 'main' of origin/main

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ function Card() {
     <div className='border-2 w-56 h-72 rounded-md m-5 mb-20 mt-10 mr-10'>
         <div className='flex border-2 h-32'>
       <div className='p-2 h-40 w-36'>
-      <img src={pokiball} alt="Girl in a jacket" width="100" height="100" className='border-2'/>
+      <img src={pokiball} alt="pokiball" width="100" height="100" className='border-2'/>
       </div>
         <div className='text-white pt-10'>Osmar Zaine</div>
         </div>
